5fd6833fe4d38b20043b5cd8ec7a7203574e7075
unknown <jhanvisdesai@gmail.com>
2021-08-01 23:16:44 +0530

    get changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'font-awesome/css/font-awesome.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Contact } from './components'
+import { Home } from './components'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/">
+          <Home />
           <Contact />
         </Route>
       </Router>
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div class="home">
+            <h1>how u doin'</h1>
+            <div></div>
+        </div>
+    )
+}
+
+export default Home
