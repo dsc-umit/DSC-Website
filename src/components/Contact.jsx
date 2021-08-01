7471172e8337489934d6e17676877e6511b4cc83
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-08-02 00:15:02 +0530

    Header completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,18 +8,6 @@ const Contact = () => {
     return (
         <div>
             <Container>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
-                <h1>boom</h1>
                 <Row>
                     <Col lg={6}>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.483121923021!2d72.82830744981766!3d19.086450178517236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x202d318309cd0d56!2sUsha%20Mittal%20Institute%20of%20Technology!5e0!3m2!1sen!2sin!4v1627838446541!5m2!1sen!2sin" width="100%" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
