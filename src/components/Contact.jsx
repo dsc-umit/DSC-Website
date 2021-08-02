348678d3d29c5599f72b5bd35bc20d3dc650da38
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-08-02 14:06:28 +0530

    Emailjs integration done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useState, React } from 'react';
+import emailjs from 'emailjs-com';
+import swal from 'sweetalert';
 import './contact.css'
-import { Container } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
@@ -10,6 +12,39 @@ import { faAddressCard, faEnvelope, faHeart, faPhoneAlt } from '@fortawesome/fre
 import { faDiscord, faFacebook, faGithub, faInstagram, faLinkedin, faMedium, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
+
+    const [templateParams, setTemplateParams] = useState({
+        subject: '',
+        email_id: '',
+		from_name: '',
+        message: ''
+	});
+
+    const onInputChange = e => {
+		setTemplateParams({...templateParams,[e.target.name]: e.target.value})
+	}
+
+    const onSubmit = async (e) => {
+		e.preventDefault();
+            emailjs.send('service_e6s2kp5','template_crdqhmt', templateParams, 'user_hNSJ8XbPIhVIgQ57FFJzw')
+                .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+                }, (err) => {
+                console.log('FAILED...', err);
+                });
+                swal({
+                    title: "Sent Successfully!",
+                    text: "Glad to hear from you! We will get back to you as soon possible",
+                    icon: "success",
+                  });
+                setTemplateParams({
+                    subject: '',
+                    email_id: '',
+                    from_name: '',
+                    message: ''
+                })
+    }
+
     return (
         <div className="contact">
             <br></br>
@@ -46,23 +81,23 @@ const Contact = () => {
                         </Row>
                     </Col>
                     <Col lg={6} sm={12} xs={12}>
-                        <Form>
+                        <Form onSubmit={e => onSubmit(e)}>
                             <Row className="contact-form">
                                 <Form.Group as={Col}>
-                                    <Form.Control className="contact-form-box" placeholder="Your Name" />
+                                    <Form.Control onChange={e => onInputChange(e)} value={templateParams.from_name} name="from_name" className="contact-form-box" placeholder="Your Name" />
                                 </Form.Group>
 
                                 <Form.Group as={Col}>
-                                    <Form.Control className="contact-form-box" type="email" placeholder="Your Email-id" />
+                                    <Form.Control onChange={e => onInputChange(e)} value={templateParams.email_id} name="email_id" className="contact-form-box" type="email" placeholder="Your Email-id" />
                                 </Form.Group>
                             </Row>
 
                             <Form.Group className="contact-form">
-                                <Form.Control className="contact-form-box" placeholder="Enter the Subject" />
+                                <Form.Control onChange={e => onInputChange(e)} value={templateParams.subject} name="subject" className="contact-form-box" placeholder="Enter the Subject" />
                             </Form.Group>
 
                             <Form.Group className="contact-form">
-                                <Form.Control className="contact-form-box" placeholder="Write your message..." as="textarea" rows={8} />
+                                <Form.Control onChange={e => onInputChange(e)} value={templateParams.message} name="message" className="contact-form-box" placeholder="Write your message..." as="textarea" rows={8} />
                             </Form.Group>
                             <Button className="contact-button" type="submit">
                                 Submit
