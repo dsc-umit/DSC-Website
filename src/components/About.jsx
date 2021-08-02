10c7fd310f6df28cb878619615220de4f181ad21
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-08-02 20:56:10 +0530

    engine trial

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Container, Image, Row, Col} from 'react-bootstrap';
 import img1 from '../media/3.png';
 import img2 from '../media/7.png';
-import "./about.css";
+import "./About.css";
 
 const About = () => {
     return (
