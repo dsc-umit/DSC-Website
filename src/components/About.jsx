7bd975e8a556ec83f5054f7af26e60c2a8138e2f
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-08-02 20:45:15 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <Container>
         <Row>
             <Col xs={12} lg={7} className="col1">
-                <h3 className="lead">About Google Developer Student Club</h3>
+                <h3 className="lead about-h">About Google Developer Student Club</h3>
                 <p>Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a DSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community. Opportunities DSCs provide students with :
                 <ul>
                 <li>Gain relevant industry experience by solving problems for local organizations with technology based solutions.</li>
@@ -32,7 +32,7 @@ const About = () => {
                </center>
             </Col>
             <Col lg={8}>
-                <h3 className="lead">About GDSC-UMIT</h3>
+                <h3 className="lead about-h">About GDSC-UMIT</h3>
                 <p>Developer Student Clubs of Usha Mittal Institute Of Technology, powered by Google Developers, is a learning community that aims to inspire intelligent minds in the field of technology. It is open to any student, ranging from amateur developers who are just starting to advanced developers. It is a community for students to connect and collaborate as they solve real-world problems.
                    <br></br>We at DSC UMIT believe in building a strong community where people can learn and grow together. Our goal is to combine new ideas with technical skills to create something innovative.</p>
             </Col>
