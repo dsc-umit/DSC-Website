2414411febf1d4e2686b84b5973504768e1a8cab
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-08-02 07:22:55 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'font-awesome/css/font-awesome.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Contact } from './components'
+import { Contact , Testimonials} from './components'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/">
+         <Testimonials/>
           <Contact />
         </Route>
       </Router>
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div>
+      <h3>TESTIMONIALS</h3>
+      <div className="cards">
+            <div className="card">
+                
+                <div className="card__info">
+                    <h3 className="card__title">Kiran Singh</h3>
+                    <span className="card__category">Former DSC Lead</span>
+                    <p>Being the DSC-UMIT lead, I had a wonderful experience! From setting up the club,
+                     to making students familiar to it, to organising workshops and seminars, DSC had a 
+                     great impact on me. During my tenure as the lead, we conducted various events on C++,
+                      Java, Android Development, Google Cloud, Design thinking, etc. DSC focuses on the 
+                      importance of community learning which allows students to experience learning in a 
+                      more realistic and practical way. To all those looking to help local businesses around you
+                       or those looking to change the world, DSC is the platform for you! </p>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <div className="cards">
+            <div className="card">
+                
+                <div className="card__info">
+                    <h3 className="card__title">Vaishnavi Desai</h3>
+                    <span className="card__category">Former Core Member</span>
+                    <p>My experience in DSC core team was an amazing one! Not only did I learn how to manage 
+                    and organize events, but also experienced the art of conducting sessions and discovered a lot
+                     of technical concepts. All in all, it was an exciting and a wonderful learning experience. </p>
+                    
+                </div>
+            </div>
+        </div>
+        <div className="cards">
+            <div className="card">
+                
+                <div className="card__info">
+                    <h3 className="card__title">Sanjana Joshi</h3>
+                    <span className="card__category">Former DSC Lead</span>
+                    <p> My experience with DSC has been quite vivid, I was exposed to various communities where I 
+                    could learn and grow. It helped me in boosting my confidence and knowledge about the trending 
+                    technologies in the market. Also, It gave me an opportunity to endure my technical capabilities
+                     to help other people. </p>
+                    
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Testimonials
